74339501d458508e5d42e352f75ababb89f8f50e
Salim Castro <salimrafaelcastro.ortiz@gmail.com>
2022-07-05 13:19:41 -0500

    Creacion de los eventos del Socket

---
+++ sockets/sockets.ts
@@ -0,0 +1,11 @@
+import SocketIO, {Socket } from 'socket.io'
+export const desconectar =  (cliente:Socket) => {
+    cliente.on('disconnect', ()=>{
+         console.log('Cliente desconectado');
+    })
+}
+export const mensaje=(cliente:Socket)=>{
+    cliente.on('mensaje', ( payload: { de: String, cuerpo: String})=>{
+    console.log('Mensaje recibido', payload)
+    })
+}
