b566de6fbae7e43a860d5302acfcad86c64247f0
Salim Castro <salimrafaelcastro.ortiz@gmail.com>
2022-07-12 14:25:08 -0500

    Diseño de Login y Configuración de Usuario

--- sockets/sockets.ts
+++ sockets/sockets.ts
@@ -1,13 +1,21 @@
 import SocketIO, {Socket } from 'socket.io'
 import socketIO from 'socket.io'
-export const desconectar =  (cliente:Socket) => {
+export const desconectar = (cliente:Socket) => {
     cliente.on('disconnect', ()=>{
          console.log('Cliente desconectado');
     })
 }
-export const mensaje=(cliente:Socket, io: socketIO.Server)=>{
+
+export const mensaje = (cliente:Socket, io: socketIO.Server)=>{
     cliente.on('mensaje', ( payload: { de: String, cuerpo: String})=>{
     console.log('Mensaje recibido', payload);
     io.emit('mensaje-nuevo', payload);
+    })
+}
+
+//Configurar Usuario
+export const configurarUsuario = (cliente:Socket, io: socketIO.Server)=>{
+    cliente.on('configurar-usuario', ( payload: { nombre: string })=>{
+    console.log('Bienvenido Usuario ', payload.nombre);
     })
 }
